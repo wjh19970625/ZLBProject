fdae769b31ded62e6ccc7e90615d915e2feebb74
王捷航 <2281837849@qq.com>
2018-12-18 17:00:09 +0800

    ss

--- app/src/main/java/com/example/zlb/ui/activity/PayActivity.java
+++ app/src/main/java/com/example/zlb/ui/activity/PayActivity.java
@@ -70,10 +70,10 @@ public class PayActivity extends ActionBarActivity {
                     // 判断resultStatus 为9000则代表支付成功
                     if (TextUtils.equals(resultStatus, "9000")) {
                         // 该笔订单是否真实支付成功，需要依赖服务端的异步通知。
-                        Toast.makeText(PayActivity.this, "支付成功", Toast.LENGTH_SHORT).show();
+                        showToast("支付成功");
                     } else {
                         // 该笔订单真实的支付结果，需要依赖服务端的异步通知。
-                        Toast.makeText(PayActivity.this, "支付失败", Toast.LENGTH_SHORT).show();
+                        showToast("支付失败");
                     }
                     break;
             }
